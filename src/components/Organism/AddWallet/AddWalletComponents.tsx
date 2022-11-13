740aea369c6250267cfa850178f807646a5e780c
MariuszRudnik <86014393+MariuszRudnik@users.noreply.github.com>
2022-11-13 09:16:37 +0100

    Add wallet  works

--- src/components/Organism/AddWallet/AddWalletComponents.tsx
+++ src/components/Organism/AddWallet/AddWalletComponents.tsx
@@ -60,47 +60,47 @@ export const AddWalletComponents: React.FC<any> = ({ addWallet }: any) => {
               initialState: Number(initialState)
             };
 
-          addWallet(values);
-          actions.setSubmitting(false);
-        }}>
-        <StylForm as={Form}>
-          <FormWrapper>
-            <label htmlFor="nameOfWallet">
-              <Paragraph>Name Wallet</Paragraph>
-              <Input
-                as={Field}
-                type="text"
-                placeholder="Name of Wallet"
-                name="nameOfWallet"
-                id="nameOfWallet"
-              />
-            </label>
+            addWallet(values);
+            actions.setSubmitting(false);
+          }}>
+          <StylForm as={Form}>
+            <FormWrapper>
+              <label htmlFor="nameOfWallet">
+                <Paragraph>Name Wallet</Paragraph>
+                <Input
+                  as={Field}
+                  type="text"
+                  placeholder="Name of Wallet"
+                  name="nameOfWallet"
+                  id="nameOfWallet"
+                />
+              </label>
 
-            <label htmlFor="typeOfCurrency">
-              <Paragraph>Type Of Currency</Paragraph>
-              <Field
-                as="select"
-                type="text"
-                placeholder="TypeOfCurrency"
-                name="typeOfCurrency"
-                id="typeOfCurrency"
-                className={cssStyle.select}>
-                <option value="PLN">PLN</option>
-                <option value="USD">USD</option>
-                <option value="EURO">EURO</option>
-              </Field>
-            </label>
-            <label htmlFor="initialState">
-              <Paragraph>Initial State</Paragraph>
-              <Input
-                as={Field}
-                type="number"
-                placeholder="Initial State"
-                name="initialState"
-                id="initialState"
-              />
-            </label>
-            <ErrorMessage name="initialState"></ErrorMessage>
+              <label htmlFor="typeOfCurrency">
+                <Paragraph>Type Of Currency</Paragraph>
+                <Field
+                  as="select"
+                  type="text"
+                  placeholder="TypeOfCurrency"
+                  name="typeOfCurrency"
+                  id="typeOfCurrency"
+                  className={cssStyle.select}>
+                  <option value="PLN">PLN</option>
+                  <option value="USD">USD</option>
+                  <option value="EURO">EURO</option>
+                </Field>
+              </label>
+              <label htmlFor="initialState">
+                <Paragraph>Initial State</Paragraph>
+                <Input
+                  as={Field}
+                  type="number"
+                  placeholder="Initial State"
+                  name="initialState"
+                  id="initialState"
+                />
+              </label>
+              <ErrorMessage name="initialState"></ErrorMessage>
 
               <ButtonWrapper>
                 <Button secondary={false} type="submit">
